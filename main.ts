cb8172d80dc84530e522371ed1c8c109cb457604
toushouha <toushouha@gmail.com>
2022-12-10 19:55:36 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (20 < input.temperature()) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+})
+basic.showIcon(IconNames.Heart)
